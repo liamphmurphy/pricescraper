687a8a25ab37b985cf23a9a19d1b64c8847b2228
murnux <liammurphy@fastmail.com>
2017-12-10 19:49:04 -0800

    Made amazonscrape.py file. Going to make separate files for each site until I start utilizing some OOP goodness.

--- amazonscrape.py
+++ amazonscrape.py
@@ -1,14 +1,17 @@
 from lxml import html
 import requests
 
+#userquery has to be in the form of an Amazon URL (with the keyword), to be worked on in the future.
 userquery = input("Enter the URL: ")
 
 page = requests.get(userquery)
 tree = html.fromstring(page.content)
 
+# Currently, the program only searches Amazon. More sites to come later!
 productname = tree.xpath('//h2[@class="a-size-medium s-inline  s-access-title  a-text-normal"]/text()')
 price = tree.xpath('//span[@class="a-offscreen"]/text()')
 
+# Cut down the list gathered by the scrape to just 5 results, mainly for testing and reducing load times.
 productname = productname[:5]
 price = price[:5]
 
